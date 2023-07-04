59310c658faa7d9973bd351ab23ad66de2398929
jewpi <nathan@jewpi.com>
2023-07-04 16:14:01 +0200

    acf editor fix update

--- supports/editor-fixes/acf.php
+++ supports/editor-fixes/acf.php
@@ -6,13 +6,13 @@ add_filter('termmeta_box_args_acf_form_taxonomy', function($args, $taxonomy, $te
 		jQuery(document).ready( function($) {
 			var acf_forms = $('.form-table tr.acf-field:first-of-type').closest('.form-table');
 			$('#acf_form_taxonomy .inside h2').each(function(i){
-				$(this).after(acf_forms.eq(i).html());
+				$(this).after('<table>' + acf_forms.eq(i).html() + '</table>');
 				acf_forms.eq(i).remove();
 			});
 			jQuery('td.acf-input').css('width','100%');
 		});
 		</script><?php
-	},9);//important to run before termmeta_boxes footer
+	}, 9);//important to run before termmeta_boxes footer
 return $args;	
 }, 10, 3);
 
